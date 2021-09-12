97bc8184167606d72bd9ae091c8eea3ed508879c
Rifqi Al Fatih <rifqialfatih@gmail.com>
2021-09-12 22:00:10 +0200

    add next prev, impl search

--- src/malloc.rs
+++ src/malloc.rs
@@ -4,8 +4,10 @@ use std::mem::size_of;
 use crate::syscalls::{BRK, syscall1};
 
 pub struct Header {
-    // used to store size and free flag for optimization
-    internal: usize
+    // Used to store size and free flag for optimization.
+    internal: usize,
+    prev: Block,
+    next: Block
 }
 
 pub struct Block (*mut Header);
@@ -14,6 +16,7 @@ pub struct Data (*mut usize);
 
 impl Header {
     pub fn get_size(&self) -> usize {
+        // & 1...1111000 on x86_64 system
         self.internal & !(size_of::<usize>() - 1)
     }
 
@@ -29,8 +32,13 @@ impl Header {
         self.internal = self.get_size() | free_bit;
     }
 
+    // Creates new header setting the free state as 0 (occupied).
     pub fn new(size: usize) -> Header {
-        Header { internal: size }
+        Header { 
+            internal: size,
+            prev: Block::null(),
+            next: Block::null()
+        }
     }
 }
 
@@ -40,9 +48,13 @@ impl Block {
         self.0 as usize == 0
     }
 
+    pub fn null() -> Block {
+        Block(0 as *mut Header)
+    }
+
     #[inline(always)]
-    pub fn from_ptr(ptr: usize) -> Block {
-        Block(ptr as *mut Header)
+    pub fn from_usize(u: usize) -> Block {
+        Block(u as *mut Header)
     }
 
     #[inline(always)]
@@ -50,14 +62,34 @@ impl Block {
         unsafe { &mut *self.0 }
     }
 
-    #[inline(always)]
-    pub fn next_block_ptr(&self) -> usize {
-        let ptr = self as *const Block as usize;
-        ptr + size_of::<Header>() + self.header().get_size()
+    pub fn next_by_total_size(&self) -> Block {
+        let addr = self as *const Block as usize;
+        let next_addr = addr + self.get_total_size();
+        Block::from_usize(next_addr)
     }
 
-    pub fn next_block(&self) -> Block {
-        Block::from_ptr(self.next_block_ptr())
+    pub fn has_next(&self) -> bool {
+        !self.header().next.is_null()
+    }
+
+    pub fn next<'a>(&'a self) -> &'a mut Block {
+        &mut self.header().next
+    }
+
+    pub fn has_prev(&self) -> bool {
+        !self.header().prev.is_null()
+    }
+
+    pub fn prev<'a>(&'a self) -> &'a mut Block {
+        &mut self.header().prev
+    }
+
+    pub fn get_data_size(&self) -> usize {
+        self.header().get_size()
+    }
+
+    pub fn get_total_size(&self) -> usize {
+        self.get_data_size() + 2*size_of::<Header>()
     }
 
     pub fn data(&self) -> Option<Data> {
@@ -70,7 +102,7 @@ impl Block {
         }
     }
 
-    // because of alignment, total size must be at least 1 word which
+    // Because of alignment, total size must be at least 1 word which
     // should have the last bit unused. This is used as free flag
     // 1 = free
     // 0 = occupied
@@ -83,26 +115,32 @@ impl Block {
         self.header().set_free_bit(free_bit);
     }
 
-    // split block, occupy, and return the first `size` of the block.
-    // no splitting occurs if `size` == size of this block.
-    // NOTE: `size` includes the size of the header
-    pub fn split<'a>(&'a mut self, size: usize) -> &'a mut Block {
-        let block_total_size = size_of::<Header>() + self.header().get_size();
+    // Split block. Occupy, and return the first of the two block.
+    // No splitting occurs if `data_size` + (2 * size_of::<Header>()) == size of this block.
+    // NOTE: `data_size` doesn't include the size of the header
+    pub fn split<'a>(&'a mut self, data_size: usize) -> &'a mut Block {
+        let old_total_size = self.get_total_size();
+        let new_total_size = data_size + (2 * size_of::<Header>());
 
-        if block_total_size == size {
+        if old_total_size == new_total_size {
             return &mut *self
         }
 
-        let data_size = size - size_of::<Header>();
-        self.header().set_size(data_size);
-        self.header().set_free_bit(0);
+        let next_block = if self.has_next() { self.next_by_total_size() } else { Block::null() };
 
-        let remaining_ptr = self.0 as usize + size;
-        let remaining_block = Block::from_ptr(remaining_ptr);
-        let remaining_total_size = block_total_size - size;
-        let remaining_data_size = remaining_total_size - size_of::<Header>();
+        let remaining_ptr = self.0 as usize + new_total_size;
+        let remaining_block = Block::from_usize(remaining_ptr);
+        let remaining_total_size = old_total_size - new_total_size;
+        let remaining_data_size = remaining_total_size - (2 * size_of::<Header>());
         remaining_block.header().set_size(remaining_data_size);
         remaining_block.header().set_free_bit(1);
+        remaining_block.header().next = next_block;
+        remaining_block.header().prev = Block::from_usize(self.0 as usize);
+
+        self.header().set_size(data_size);
+        self.header().set_free_bit(0);
+        self.header().next = remaining_block;
+
         &mut *self
     }
 }
@@ -111,27 +149,40 @@ enum SearchStrategy {
     FirstFit
 }
 
-// align size to a multiple of machine word 
+// Align size to a multiple of machine word.
 fn align(size: usize) -> usize {
     (size + (size_of::<usize>() - 1)) & !(size_of::<usize>() - 1)
 }
 
-fn search_free_spot(size: usize, search_strategy: SearchStrategy) -> Option<Block> {
+// Search blocks for free spot or return the last block.
+// Caller must check the `bool == true` flag if it found spot, otherwise `Block` is the last block.
+fn search_free_spot_or_last(size: usize, search_strategy: SearchStrategy) -> (Block, bool) {
     match search_strategy {
         SearchStrategy::FirstFit => search_first_fit(size)
     }
 }
 
-fn search_first_fit(size: usize) -> Option<Block> {
-    None
+fn search_first_fit(size: usize) -> (Block, bool) {
+    let mut current = unsafe { &root };
+    let mut found = false;
+
+    while current.has_next() {
+        current = current.next();
+        if current.is_free() && current.get_total_size() >= size {
+            found = true;
+            break;
+        }
+    }
+
+    (Block::from_usize(current.0 as usize), found)
 }
 
-static mut root: Block = Block(0 as *mut Header);
+pub static mut root: Block = Block(0 as *mut Header);
 static mut current_brk: *mut usize = 0 as *mut usize;
 
 unsafe fn brk(end_data_segment: *mut usize) -> *mut isize {
     let new = syscall1(BRK, end_data_segment as usize);
-    println!("eds {:?} new {:?} newisize {:?} asas {:?}", end_data_segment as usize, new, new as isize, new as isize as *mut isize as usize);
+    // println!("eds {:?} new {:?} newisize {:?} asas {:?}", end_data_segment as usize, new, new as isize, new as isize as *mut isize as usize);
     current_brk = new as *mut usize;
     // allow brk to set over end, also handles 0
     (if new < (end_data_segment as usize) {-1} else {new as isize}) as *mut isize
@@ -139,54 +190,63 @@ unsafe fn brk(end_data_segment: *mut usize) -> *mut isize {
 
 unsafe fn sbrk(increment: usize) -> *mut isize {
     if current_brk as usize == 0 {
-        println!("if {:?}", brk(0 as *mut usize) as usize);
+        let temp = brk(0 as *mut usize) as usize;
+        // println!("if {:?}", temp);
     }
     let new = current_brk as usize + increment;
     let res = brk(new as *mut usize);
-    println!("res {:?}", res as usize);
+    // println!("res {:?}", res as usize);
     res
 }
 
 pub fn init_malloc() {
     unsafe {
-        println!("C1");
+        // println!("C1");
         let current = brk(0 as *mut usize);
-        println!("current {:?}", current as usize);
-        root = Block::from_ptr(current as usize);
+        // println!("current {:?}", current as usize);
+        root = Block::from_usize(current as usize);
         let after = sbrk(size_of::<Header>());
-        println!("after {:?}", after as usize);
+        // println!("after {:?}", after as usize);
         *root.header() = Header::new(0);
-        println!("C4");
+        // println!("C4");
     }
 }
 
 pub fn malloc(size: usize) -> *mut usize {
     assert!(size > 0);
 
-    let total_size = align(size_of::<Header>() + size);
+    let aligned_size = align(size);
+    let total_size = aligned_size + (2 * size_of::<Header>());
 
     let current_root = unsafe { &root };
-    if (current_root.is_null()) {
+    if current_root.is_null() {
         init_malloc();
     }
 
-    let spot: Option<Block> = search_free_spot(total_size, SearchStrategy::FirstFit);
+    let (mut block, found) = search_free_spot_or_last(total_size, SearchStrategy::FirstFit);
 
-    match spot {
-        Some(mut free_block) => {
-            // reuse old block
-            let new = free_block.split(total_size);
+    if found {
+        // `block` can be reuse
+        let new = block.split(total_size);
 
-            new.data().unwrap().0
-        },
-        None => {
-            // allocate new memory
-            let current = unsafe { sbrk(0) as *mut usize };
-            let after = unsafe { sbrk(total_size) as *mut usize };
+        new.data().unwrap().0
+    } else {
+        // `block` is the last Block, allocate new memory
+        let current = unsafe { sbrk(0) as *mut usize };
+        let after = unsafe { sbrk(total_size) as *mut usize };
 
-            let new = Block::from_ptr(current as usize);
-            new.data().unwrap().0
+        let new = Block::from_usize(current as usize);
+        unsafe {
+            *new.0 = Header {
+                internal: aligned_size,
+                prev: Block::from_usize(block.0 as usize),
+                next: Block::null()
+            };
         }
+        
+        block.header().next = Block::from_usize(current as usize);
+
+        new.data().unwrap().0
     }
 }
 
@@ -206,6 +266,7 @@ mod tests {
 
     #[test]
     fn test_align() {
+        // test only applies on x86_64
         assert_eq!(0, align(0));
         assert_eq!(8, align(1));
         assert_eq!(8, align(7));
@@ -217,11 +278,11 @@ mod tests {
     }
 
     #[test]
-    fn test_block_from_ptr() {
+    fn test_block_from_usize() {
         let header = Header::new(16);
 
         let ptr = &header as *const Header as usize;
-        let block = Block::from_ptr(ptr);
+        let block = Block::from_usize(ptr);
 
         assert_eq!(block.header().get_size(), 16);
     }
@@ -234,21 +295,22 @@ mod tests {
         assert_eq!(16, block.header().get_size());
     }
 
-    #[test]
-    fn test_block_next_block() {
-        let header = Header::new(8);
+    // #[test]
+    // fn test_block_next() {
+    //     let header = Header::new(8);
 
-        let block = Block(&header as *const Header as *mut Header);
-        let next_block_ptr = block.next_block_ptr() as *mut Header;
-        unsafe {
-            *next_block_ptr = Header::new(16);
-        }
-        let next_block = Block(next_block_ptr);
-        assert_eq!(block.next_block().header().get_size(), next_block.header().get_size())
-    }
+    //     let block = Block(&header as *const Header as *mut Header);
+    //     let next_block_ptr = (block.0 as usize + block.get_total_size()) as *mut Header;
+    //     unsafe {
+    //         *next_block_ptr = Header::new(16);
+    //     }
+    //     let next_block = Block(next_block_ptr);
+    //     block.header().next = next_block;
+    //     assert_eq!(block.next().header().get_size(), next_block.header().get_size())
+    // }
 
     #[test]
     fn test_malloc() {
-        malloc(16);
+        malloc(3);
     }
 }
